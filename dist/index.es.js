32f3c33070571eaaccde5eb6edc3c9de9e1b0a2c
kanda-ls <kanda-ls>
2025-10-26 12:22:16 +0700

    Fix sidebar dropdown with fixed positioning

--- dist/index.es.js
+++ dist/index.es.js
@@ -5940,7 +5940,7 @@ function Qn({
         {
           align: "start",
           side: "top",
-          className: "w-56 ml-4",
+          className: "w-56 !fixed !bottom-20 !left-4 !top-auto",
           children: [
             /* @__PURE__ */ r.jsxs(G, { children: [
               /* @__PURE__ */ r.jsx(ke, { className: "mr-2 h-4 w-4" }),
